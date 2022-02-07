db00042edc73825308f0250d59964b4557f68a93
Keven Lambert <2065551@cegepba.qc.ca>
2022-02-07 11:42:14 -0500

    Ajout IFileInfo

--- Project_SuperDir/Project_SuperDir/functions.cpp
+++ Project_SuperDir/Project_SuperDir/functions.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
+#include "IFileInfo.h"
 
-//IFileInfo** FindFiles(const char* aFolder) {
-//
-//}
+IFileInfo** FindFiles(const char* aFolder) {
+	IFileInfo** lTab =  new lTab*[100];
+}
